b1c532f027c36e8a72905cf555b89a50338ffeb8
Malte Lüken <malte_lueken@arcor.de>
2022-06-21 14:48:35 +0200

    Modify pipeline class to fit refactored video processing

--- mexca/core/pipeline.py
+++ mexca/core/pipeline.py
@@ -1,72 +1,29 @@
 """ Pipeline class and methods """
 
+from mexca.core.exceptions import PipelineError
 from mexca.core.output import Multimodal
 
 class Pipeline:
-    enabled_video: bool
-    enabled_audio: bool
-    enabled_text: bool
-
-    filepath = None
-
-    _video_class = None
-    _audio_class = None
-    _text_class = None
-
-    def __init__(self,
-        enabled_video,
-        enabled_audio,
-        enabled_text,
-    ) -> 'Pipeline':
-        self.enabled_video = enabled_video
-        self.enabled_audio = enabled_audio
-        self.enabled_text = enabled_text
-
-        if self.get_enabled_video() and self._video_class is not None:
-            self.video = self._video_class()
-
-        if self.get_enabled_audio() and self._audio_class is not None:
-            self.audio = self._audio_class()
-
-        if self.get_enabled_text() and self._text_class is not None:
-            self.text = self._text_class()
+    def __init__(self, video=None, audio=None, text=None) -> 'Pipeline':
+        if text and not audio:
+            raise PipelineError('Cannot initialize a "text" component when no "audio" component was specified')
+        self.video = video
+        self.audio = audio
+        self.text = text
 
 
-    def get_enabled_video(self) -> bool:
-        return self.enabled_video
-
-
-    def get_enabled_audio(self) -> bool:
-        return self.enabled_audio
-
-
-    def get_enabled_text(self) -> bool:
-        return self.enabled_text
-
-
-    def set_filepath(self, filepath) -> None:
-        self.filepath = filepath
-
-
-    def get_filepath(self) -> str:
-        if self.filepath is None:
-            raise AttributeError('Attribute "filepath" must be set before it can be accessed')
-        return self.filepath
-
-
-    def apply(self, filepath) -> 'Mexca':
-
-        if self.get_enabled_video():
+    def apply(self, filepath) -> 'Multimodal':
+        if self.video:
             video_result = self.video.apply(filepath)
         else:
             video_result = None
 
-        if self.get_enabled_audio():
+        if self.audio:
             audio_result = self.audio.apply(filepath)
         else:
             audio_result = None
 
-        if self.get_enabled_text():
+        if self.text:
             text_result = self.text.apply(filepath)
         else:
             text_result = None
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,55 +1,21 @@
-""" Test Pipeline class and methods """
+""" Test pipeline class and methods """
 
 import os
-import pytest
+from mexca.video.extraction import FaceExtractor
 from mexca.core.output import Multimodal
 from mexca.core.pipeline import Pipeline
 
 class TestPipeline:
-    obj = Pipeline(
-        enabled_video=True,
-        enabled_audio=True,
-        enabled_text=True
+    pipeline = Pipeline(
+        video=FaceExtractor(
+            min_clusters=1,
+            max_clusters=4
+        )
+    )
+    filepath = filepath = os.path.join(
+        'tests', 'video_files', 'test_video_multi_5_frames.mp4'
     )
 
-    filepath = os.path.join('test', 'path')
-
-    obj_no_filepath = Pipeline(
-        enabled_video=True,
-        enabled_audio=True,
-        enabled_text=True
-    )
-
-    result = Multimodal(None, None, None)
-
-    def test_get_enabled_video(self):
-        assert self.obj.get_enabled_video()
-
-
-    def test_get_enabled_audio(self):
-        assert self.obj.get_enabled_audio()
-
-
-    def test_get_enabled_text(self):
-        assert self.obj.get_enabled_text()
-
-
-    def test_set_filepath(self):
-        self.obj.set_filepath(self.filepath)
-        assert self.obj.filepath == self.filepath
-
-
-    def test_get_filepath(self):
-        self.obj.set_filepath(self.filepath)
-        assert self.obj.get_filepath() == self.filepath
-
-        with pytest.raises(AttributeError):
-            self.obj_no_filepath.get_filepath()
-
-
-    @pytest.mark.skip(
-        reason='Subpipelines for modalities are not implemented yet; cannot apply entire pipeline'
-    )
     def test_apply(self):
-        result_pipeline = self.obj.apply(self.filepath)
-        assert result_pipeline == self.result
+        pipeline_result = self.pipeline.apply(self.filepath)
+        assert isinstance(pipeline_result, Multimodal)
