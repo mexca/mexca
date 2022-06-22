db9a01edb9868b5b177e7a51d26337abc4426562
Malte Lüken <malte_lueken@arcor.de>
2022-06-22 09:58:46 +0200

    Split unit tests for video extraction

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -7,14 +7,7 @@ import pandas as pd
 import pytest
 import torch
 from mexca.video.extraction import FaceExtractor
-
-def make_reproducible(seed):
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.set_num_threads(1)
+from moviepy.editor import VideoFileClip
 
 class TestFaceExtractor:
     extractor = FaceExtractor(min_clusters=1, max_clusters=4)
@@ -26,7 +19,60 @@ class TestFaceExtractor:
         ), 'r') as file:
         features = json.loads(file.read())
 
+
+    def test_detect(self):
+        with VideoFileClip(self.filepath, audio=False) as clip:
+            features = {
+                'box': [],
+                'prob': []
+            }
+            for frame in clip.iter_frames():
+                _, boxes, probs = self.extractor.detect(frame)
+                for box, prob in zip(boxes, probs):
+                    features['box'].append(box.tolist())
+                    features['prob'].append(prob)
+
+            assert features['box'] == self.features['box']
+            assert features['prob'] == self.features['prob']
+
+
+    def test_identify(self):
+        with VideoFileClip(self.filepath, audio=False) as clip:
+            feature_labels = []
+            for frame in clip.iter_frames():
+                faces, _, _ = self.extractor.detect(frame)
+
+                frame_labels = self.extractor.identify(faces)
+                for label in frame_labels:
+                    feature_labels.append(label)
+
+            assert feature_labels == self.features['label']
+
+
+    def test_extract(self):
+        with VideoFileClip(self.filepath, audio=False) as clip:
+            features = {
+                'landmarks': [],
+                'aus': []
+            }
+            for frame in clip.iter_frames():
+                _, boxes, _ = self.extractor.detect(frame)
+                landmarks, aus = self.extractor.extract(frame, boxes)
+                landmarks_np = np.array(landmarks).squeeze()
+                for landmark, au in zip(landmarks_np, aus):
+                    features['landmarks'].append(landmark)
+                    features['aus'].append(au)
+
+            assert np.array(features['landmarks']).shape == np.array(self.features['landmarks']).shape
+            assert np.array(features['aus']).shape == np.array(self.features['aus']).shape
+
+
     def test_apply(self):
-        make_reproducible(2022)
         features = self.extractor.apply(self.filepath)
-        assert pd.DataFrame(features).to_json() == pytest.approx(self.features, rel=1e-3)
+        assert features['frame'] == self.features['frame']
+        assert features['time'] == self.features['time']
+        assert list(map(lambda x: x.tolist(), features['box'])) == self.features['box']
+        assert features['prob'] == self.features['prob']
+        assert features['label'] == self.features['label']
+        assert np.array(features['landmarks']).shape == np.array(self.features['landmarks']).shape
+        assert np.array(features['aus']).shape == np.array(self.features['aus']).shape
