da1d0afb195742fae93dfc68d9895abfdfacd5b6
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2022-06-23 15:53:02 +0200

    initial commit for implementation of audio to text transcription

---
+++ mexca/text/transcription.py
@@ -0,0 +1,22 @@
+""" Audio to text transcription classes and methods """
+
+from huggingsound import SpeechRecognitionModel
+from mexca.core.exceptions import ModelTranscriberInitError
+
+class AudioTranscriber:
+
+    def __init__(self,language=None) -> 'AudioTranscriber':
+        self.language = language
+
+        if language == 'dutch':
+          self._pipeline = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-dutch")
+        elif language == 'english':
+          self._pipeline = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english")
+        else:
+          raise ModelTranscriberInitError("Invalid language. Please specify either 'dutch' or 'english'")
+
+    def apply(self, filepath):
+
+        transcription = self._pipeline.transcribe(filepath)
+
+        return transcription[0]['transcription']
---
+++ tests/test_audio_transcription.py
@@ -0,0 +1,25 @@
+""" Test Audio to text transcription classes and methods """
+
+import os
+from mexca.text.transcription import AudioTranscriber
+
+class TestAudioTranscription:
+    audio_transcriber = AudioTranscriber(language='english')
+    filepath = os.path.join('tests', 'audio_files', 'test_eng_5_seconds.wav')
+
+    with open(os.path.join('tests', 'reference_files', 'transcription_eng_5_seconds.txt'), 'r') as file:
+        ref_transcription = file.read().replace('\n', '')
+
+    def test_apply(self):
+        transcription = self.audio_transcriber.apply(self.filepath)
+        assert are_equal(transcription, self.ref_transcription)
+
+
+def are_equal(transcription1, transcription2):
+
+    reference_transcription = transcription1
+    predicted_transcription = transcription2
+
+    equal = (reference_transcription == predicted_transcription)
+
+    return equal
