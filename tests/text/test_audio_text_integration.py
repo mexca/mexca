8213a8c0ff9a7aea8e0f60ee6fa4d042b8beb33d
maltelueken <malte_lueken@arcor.de>
2022-07-06 16:05:10 +0200

    Allow for different word predictions in text processing tests

--- tests/text/test_audio_text_integration.py
+++ tests/text/test_audio_text_integration.py
@@ -4,7 +4,6 @@ import os
 import json
 from mexca.text.transcription import AudioTextIntegrator
 from mexca.text.transcription import AudioTranscriber
-from mexca.audio.speaker_id import SpeakerIdentifier
 
 class TestAudioTextIntegration:
     audio_text_integrator = AudioTextIntegrator(
@@ -18,9 +17,8 @@ class TestAudioTextIntegration:
         text_audio_transcription = json.loads(file.read())
 
     def test_apply(self):
-
         out  = self.audio_text_integrator.apply(self.filepath, self.text_audio_transcription['time'])
         assert all(out['text_token_id'] == self.text_audio_transcription['text_token_id'])
-        assert all(out['text_token'] == self.text_audio_transcription['text_token'])
+        assert all([token in ['', 'maak', 'en', 'er', 'groen', 'als'] for token in out['text_token']])
         assert all(out['text_token_start'] == self.text_audio_transcription['text_token_start'])
         assert all(out['text_token_end'] == self.text_audio_transcription['text_token_end'])
--- tests/text/test_audio_transcription.py
+++ tests/text/test_audio_transcription.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+import pytest
 from mexca.text.transcription import AudioTranscriber
 
 class TestAudioTranscription:
@@ -9,10 +10,12 @@ class TestAudioTranscription:
     filepath = os.path.join('tests', 'test_files', 'test_dutch_5_seconds.wav')
 
     with open(os.path.join(
-            'tests', 'reference_files', 'transcription_dutch_1_second.json'), 'r') as file:
+            'tests', 'reference_files', 'transcription_dutch_5_seconds.json'), 'r') as file:
         ref_transcription = json.loads(file.read())
 
     def test_apply(self):
         transcription = self.audio_transcriber.apply(self.filepath)
-        assert transcription['transcription'] == self.ref_transcription['transcription']
-
+        assert all(token in ['', 'maak', 'en', 'er', 'groen', 'als'] for token in transcription['transcription'].split(' '))
+        assert transcription['start_timestamps'] == self.ref_transcription['start_timestamps']
+        assert transcription['end_timestamps'] == self.ref_transcription['end_timestamps']
+        assert pytest.approx(transcription['probabilities'], rel = 1e-2) == self.ref_transcription['probabilities']
