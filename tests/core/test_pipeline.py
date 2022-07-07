10a1fc1790bdaf091c9dc4fdaf3db5a2ece85034
maltelueken <malte_lueken@arcor.de>
2022-07-07 10:52:51 +0200

    Add test skip for from default pipeline constructor on windows

--- tests/core/test_pipeline.py
+++ tests/core/test_pipeline.py
@@ -2,6 +2,8 @@
 
 import json
 import os
+import platform
+import pytest
 from mexca.core.output import Multimodal
 from mexca.core.pipeline import Pipeline
 from mexca.video.extraction import FaceExtractor
@@ -9,7 +11,7 @@ from mexca.video.extraction import FaceExtractor
 
 class TestPipeline:
     pipeline = Pipeline(
-        video=FaceExtractor( # Only use video because test file contains no audio
+        video=FaceExtractor(  # Only use video because test file contains no audio
             min_clusters=1,
             max_clusters=4
         ),
@@ -20,8 +22,8 @@ class TestPipeline:
         'tests', 'test_files', 'test_video_multi_5_frames.mp4'
     )
     with open(os.path.join(
-            'tests', 'reference_files', 'features_video_multi_5_frames.json'
-        ), 'r', encoding="utf-8") as file:
+        'tests', 'reference_files', 'features_video_multi_5_frames.json'
+    ), 'r', encoding="utf-8") as file:
         features = json.loads(file.read())
 
     def test_apply(self):
@@ -30,7 +32,8 @@ class TestPipeline:
         # Only test time feature because the others are covered elsewhere
         assert pipeline_result.features['time'] == self.features['time']
 
-
+    @pytest.mark.skipif(platform.system() == 'Windows',
+                        reason='Windows VMs run out of memory when loading entire pipeline')
     def test_from_default(self):
         pipeline = self.pipeline.from_default()
         assert isinstance(pipeline, Pipeline)
