b184255bb686b2ab098a88324c12ac4b7e7f141d
maltelueken <malte_lueken@arcor.de>
2022-08-03 16:52:28 +0200

    Add tests for audio extraction, text transcription, and core pipeline

--- tests/core/test_pipeline.py
+++ tests/core/test_pipeline.py
@@ -1,39 +1,40 @@
 """ Test pipeline class and methods """
 
-import json
 import os
-import platform
-import pytest
+# import platform
+# import pytest
+from mexca.audio.extraction import VoiceExtractor
+from mexca.audio.identification import SpeakerIdentifier
+from mexca.audio.integration import AudioIntegrator
 from mexca.core.output import Multimodal
 from mexca.core.pipeline import Pipeline
+from mexca.text.transcription import AudioTextIntegrator
+from mexca.text.transcription import AudioTranscriber
 from mexca.video.extraction import FaceExtractor
 
 
 class TestPipeline:
     pipeline = Pipeline(
-        video=FaceExtractor(  # Only use video because test file contains no audio
-            min_clusters=1,
-            max_clusters=4
+        video=FaceExtractor(min_clusters=1, max_clusters=3),
+        audio=AudioIntegrator(
+            SpeakerIdentifier(),
+            VoiceExtractor()
         ),
-        audio=None,
-        text=None
+        text=AudioTextIntegrator(
+            audio_transcriber=AudioTranscriber(language='english')
+        )
     )
     filepath = os.path.join(
-        'tests', 'test_files', 'test_video_multi_5_frames.mp4'
+        'tests', 'test_files', 'test_video_audio_5_seconds.mp4'
     )
-    with open(os.path.join(
-        'tests', 'reference_files', 'features_video_multi_5_frames.json'
-    ), 'r', encoding="utf-8") as file:
-        features = json.loads(file.read())
 
     def test_apply(self):
         pipeline_result = self.pipeline.apply(self.filepath)
+        # Only test if pipeline completes because the features are covered elsewhere
         assert isinstance(pipeline_result, Multimodal)
-        # Only test time feature because the others are covered elsewhere
-        assert pipeline_result.features['time'] == self.features['time']
 
-    @pytest.mark.skipif(platform.system() == 'Windows',
-                        reason='Windows VMs run out of memory when loading entire pipeline')
+    # @pytest.mark.skipif(platform.system() == 'Windows',
+    #                    reason='Windows VMs run out of memory when loading entire pipeline')
     def test_from_default(self):
         pipeline = self.pipeline.from_default()
         assert isinstance(pipeline, Pipeline)
--- tests/text/test_audio_transcription.py
+++ tests/text/test_audio_transcription.py
@@ -2,10 +2,12 @@
 
 import json
 import os
+import pytest
+from mexca.core.exceptions import ModelTranscriberInitError
 from mexca.text.transcription import AudioTranscriber
 
 
-class TestAudioTranscription:
+class TestAudioTranscriptionDutch:
     audio_transcriber = AudioTranscriber(language='dutch')
     filepath = os.path.join('tests', 'test_files', 'test_dutch_5_seconds.wav')
 
@@ -21,4 +23,29 @@ class TestAudioTranscription:
         assert transcription['start_timestamps'] == self.ref_transcription['start_timestamps']
         assert transcription['end_timestamps'] == self.ref_transcription['end_timestamps']
         # Large difference between probabilities across different os
-        #assert pytest.approx(transcription['probabilities'], rel = 1e-2) == self.ref_transcription['probabilities']
+        # assert pytest.approx(transcription['probabilities'], rel = 1e-2) == self.ref_transcription['probabilities']
+
+
+    def test_model_transcriber_init_error(self):
+        with pytest.raises(ModelTranscriberInitError):
+            AudioTranscriber(language=None)
+
+
+class TestAudioTranscriptionEnglish:
+    audio_transcriber = AudioTranscriber(language='english')
+    filepath = os.path.join('tests', 'test_files', 'test_eng_5_seconds.wav')
+
+    with open(
+        os.path.join('tests', 'reference_files', 'transcription_eng_5_seconds.json'),
+        'r', encoding="utf-8") as file:
+        ref_transcription = json.loads(file.read())
+
+    def test_apply(self):
+        transcription = self.audio_transcriber.apply(self.filepath)
+        assert all(token in ['senator', 'tom', 'carpor', 'down', 'there', 'and', 'i',
+                                'tik', 'think', 'senator', 'coons', 'is']
+                   for token in transcription['transcription'].split(' '))
+        assert transcription['start_timestamps'] == self.ref_transcription['start_timestamps']
+        assert transcription['end_timestamps'] == self.ref_transcription['end_timestamps']
+        # Large difference between probabilities across different os
+        # assert pytest.approx(transcription['probabilities'], rel = 1e-2) == self.ref_transcription['probabilities']
