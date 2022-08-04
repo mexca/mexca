85a9002df51cff56d6b3490a524d51f9cd75adb6
maltelueken <malte_lueken@arcor.de>
2022-08-04 13:48:18 +0200

    Add test for matching face ids and speaker ids

--- tests/core/test_output.py
+++ tests/core/test_output.py
@@ -24,3 +24,18 @@ class TestMultimodal:
         multi.add({'time': [0.0, 0.2]}, replace=True)
 
         assert multi.features == {'time': [0.0, 0.2]}
+
+    @staticmethod
+    def test_match_faces_speakers():
+        multi = Multimodal()
+        multi.add(
+            feature_dict = {
+                'time': [0.1, 0.3, 0.4, 0.5],
+                'face_id': [2, 2, 3, 3],
+                'speaker_id': ['A', 'A', 'B', 'B']
+            }
+        )
+        multi.match_faces_speakers()
+
+        # Test set due to switching id assignment
+        assert set(multi.features['match_id'].tolist()) == set([1.0, 1.0, 2.0, 2.0])
