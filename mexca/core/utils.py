024b933c62b523d487bdfc0aeef5601740f5463b
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2022-08-12 15:45:10 +0200

    revert "raised warning"
    This reverts commit cc21878b9b2b0435e9914f21f2751a94ff456d8e.

--- mexca/core/utils.py
+++ mexca/core/utils.py
@@ -27,7 +27,7 @@ def create_time_var_from_step(time_step, end_time):
     not_processed = Decimal(end_time)%Decimal(time_step) # Use 'Decimal' to avoid issues with float representation
 
     if not_processed > 0.0:
-        raise TimeStepWarning(f'Length of file is not a multiple of "time_step": {not_processed}s at the end of the file will not be processed')
+        TimeStepWarning(f'Length of file is not a multiple of "time_step": {not_processed}s at the end of the file will not be processed')
 
     time = np.arange(start=0.0, stop=end_time, step=time_step)
 
