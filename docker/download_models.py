1590e95cfc9cc7cfd0eff8b5c505e14fdd92ec72
maltelueken <malte_lueken@arcor.de>
2023-01-10 14:07:46 +0100

    Update model download script

--- docker/download_models.py
+++ docker/download_models.py
@@ -5,8 +5,43 @@ container so they won't be downloaded every time the container is run.
 
 """
 
-from mexca.core.pipeline import Pipeline
+import argparse
 
-# Trigger model download by creating Pipeline instance
-print('Download pretrained models...')
-pipeline = Pipeline().from_default()
+def cli():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--component', type=str, default='all')
+
+    args = parser.parse_args()
+
+    print('Downloading pretrained models...')
+
+    if args.component == 'vid':
+        from mexca.video import FaceExtractor
+
+        component = FaceExtractor(num_faces=2)
+
+    elif args.component == 'spe':
+        from mexca.audio import SpeakerIdentifier
+
+        component = SpeakerIdentifier()
+
+    elif args.component == 'voi':
+        pass
+
+    elif args.component == 'tra':
+        from mexca.text import AudioTranscriber
+
+        component = AudioTranscriber
+
+    elif args.component == 'sen':
+        from mexca.text import SentimentExtractor
+
+        component = SentimentExtractor
+
+    else:
+        raise Exception("Please specify a valid component: 'vid', 'spe', 'voi', 'tra', 'sen'")
+
+
+if __name__ == '__main__':
+    cli()
