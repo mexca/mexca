db5ac8182fade11bf770dfc6187df179c73c53b5
maltelueken <malte_lueken@arcor.de>
2023-01-18 15:33:03 +0100

    Add result validation to pipeline tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -5,6 +5,7 @@ from mexca.audio import SpeakerIdentifier, VoiceExtractor
 from mexca.data import AudioTranscription, Multimodal, SentimentAnnotation, SpeakerAnnotation, VideoAnnotation, VoiceFeatures
 from mexca.pipeline import Pipeline
 from mexca.text import AudioTranscriber, SentimentExtractor
+from mexca.utils import _validate_multimodal
 from mexca.video import FaceExtractor
 
 
@@ -95,27 +96,7 @@ class TestPipeline:
             keep_audiofile=True # Otherwise test audio file is removed
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.video_annotation, VideoAnnotation)
-        assert isinstance(result.audio_annotation, SpeakerAnnotation)
-        assert isinstance(result.voice_features, VoiceFeatures)
-        assert isinstance(result.transcription, AudioTranscription)
-        assert isinstance(result.sentiment, SentimentAnnotation)
-        assert isinstance(result.features, pd.DataFrame)
-
-        assert result.features.frame.le(125).all() and result.features.frame.ge(0).all()
-        assert result.features.time.le(5.0).all() and result.features.time.ge(0.0).all()
-
-        assert result.features.segment_start.le(result.features.time, fill_value=0).all()
-        assert result.features.segment_end.ge(result.features.time, fill_value=result.features.time.max()).all()
-        assert result.features.segment_start.dropna().lt(result.features.segment_end.dropna()).all()
-        assert result.features.segment_start.isna().eq(result.features.segment_end.isna()).all()
-        assert result.features.segment_start.isna().eq(result.features.segment_speaker_label.isna()).all()
-
-        assert result.features.span_start.le(result.features.time, fill_value=0).all()
-        assert result.features.span_end.ge(result.features.time, fill_value=result.features.time.max()).all()
-        assert result.features.span_start.isna().eq(result.features.span_end.isna()).all()
-        assert result.features.span_start.isna().eq(result.features.span_text.isna()).all()
+        _validate_multimodal(result)
 
 
     def test_face_extractor_pipeline(self, face_extractor_pipeline):
@@ -126,9 +107,12 @@ class TestPipeline:
             keep_audiofile=True
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.video_annotation, VideoAnnotation)
-        assert isinstance(result.features, pd.DataFrame)
+        _validate_multimodal(result,
+            check_audio_annotation=False,
+            check_voice_features=False,
+            check_transcription=False,
+            check_sentiment=False
+        )
 
 
     def test_speaker_identifier_pipeline(self, speaker_identifier_pipeline):
@@ -137,9 +121,12 @@ class TestPipeline:
             keep_audiofile=True # Otherwise test audio file is removed
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.audio_annotation, SpeakerAnnotation)
-        assert isinstance(result.features, pd.DataFrame)
+        _validate_multimodal(result,
+            check_video_annotation=False,
+            check_voice_features=False,
+            check_transcription=False,
+            check_sentiment=False
+        )
 
 
     def test_voice_extractor_pipeline(self, voice_extractor_pipeline):
@@ -148,9 +135,12 @@ class TestPipeline:
             keep_audiofile=True # Otherwise test audio file is removed
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.voice_features, VoiceFeatures)
-        assert isinstance(result.features, pd.DataFrame)
+        _validate_multimodal(result,
+            check_video_annotation=False,
+            check_audio_annotation=False,
+            check_transcription=False,
+            check_sentiment=False
+        )
 
 
     def test_speaker_identifier_transcription_pipeline(self, speaker_identifier_transcription_pipeline):
@@ -159,9 +149,11 @@ class TestPipeline:
             keep_audiofile=True # Otherwise test audio file is removed
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.transcription, AudioTranscription)
-        assert isinstance(result.features, pd.DataFrame)
+        _validate_multimodal(result,
+            check_video_annotation=False,
+            check_voice_features=False,
+            check_sentiment=False
+        )
 
 
     def test_speaker_identifier_transcription_sentiment_pipeline(self, speaker_identifier_transcription_sentiment_pipeline):
@@ -170,6 +162,7 @@ class TestPipeline:
             keep_audiofile=True # Otherwise test audio file is removed
         )
 
-        assert isinstance(result, Multimodal)
-        assert isinstance(result.sentiment, SentimentAnnotation)
-        assert isinstance(result.features, pd.DataFrame)
+        _validate_multimodal(result,
+            check_video_annotation=False,
+            check_voice_features=False
+        )
