8dd09476d1da7fd720b453a73fceeded2fb56db3
maltelueken <malte_lueken@arcor.de>
2023-03-17 11:41:17 +0100

    Change conditon for VoiceExtractor import to librosa

--- mexca/audio/__init__.py
+++ mexca/audio/__init__.py
@@ -9,7 +9,7 @@ all = []
 if importlib.util.find_spec('pyannote') is not None:
     from .identification import SpeakerIdentifier
     all.append('SpeakerIdentifier')
-if importlib.util.find_spec('parselmouth') is not None:
+if importlib.util.find_spec('librosa') is not None:
     from .extraction import VoiceExtractor
     all.append('VoiceExtractor')
 
