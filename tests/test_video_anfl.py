4cb653c561f3d75b05e491891c6bf7f6ae008463
maltelueken <malte_lueken@arcor.de>
2023-05-30 14:35:45 +0200

    Add tests for video ANFL module

---
+++ tests/test_video_anfl.py
@@ -0,0 +1,149 @@
+"""Test video ANFL classes and methods.
+"""
+
+import pytest
+import torch
+from mexca.video.anfl import ANFL, AUFeatureGenerator, FacialGraphGenerator, GNN, LinearBlock, MEFARG
+
+class TestLinearBlock:
+    in_features = 5
+    out_features = 5
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, self.in_features, self.in_features))
+
+    @pytest.fixture
+    def linear_block_in_eq_out(self):
+        return LinearBlock(in_features=self.in_features)
+
+    @pytest.fixture
+    def linear_block(self):
+        return LinearBlock(in_features=self.in_features, out_features=self.out_features)
+    
+
+    def test_forward_in_eq_out(self, linear_block_in_eq_out, inputs):
+        outputs = linear_block_in_eq_out.forward(inputs)
+        assert outputs.shape == inputs.shape
+
+
+    def test_forward(self, linear_block, inputs):
+        outputs = linear_block.forward(inputs)
+        assert outputs.shape == inputs.shape
+
+
+class TestGNN:
+    in_features = 5
+    n_nodes = 3
+    n_neighbors = 1
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, self.n_nodes, self.in_features))
+
+    @pytest.fixture
+    def adj_mat(self):
+        return torch.rand((1, self.n_nodes, self.n_nodes))
+
+    @pytest.fixture
+    def gnn(self):
+        return GNN(self.in_features, self.n_nodes, self.n_neighbors)
+    
+
+    def test_calc_adj_mat(self, gnn, inputs):
+        adj_mat = gnn._calc_adj_mat(inputs, self.n_neighbors)
+        assert adj_mat.shape == (1, self.n_nodes, self.n_nodes)
+
+
+    def test_normalize_digraph(self, gnn, adj_mat):
+        norm_adj_mat = gnn._normalize_digraph(adj_mat)
+        assert norm_adj_mat.shape == adj_mat.shape
+
+
+    def test_forward(self, gnn, inputs):
+        with torch.no_grad():
+            outputs = gnn.forward(inputs)
+        assert outputs.shape == inputs.shape
+
+
+class TestAUFeatureGenerator:
+    in_features = 5
+    out_features = 3
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, 2, self.in_features))
+    
+    @pytest.fixture
+    def generator(self):
+        return AUFeatureGenerator(self.in_features, self.out_features)
+    
+
+    def test_forward(self, generator, inputs):
+        with torch.no_grad():
+            outputs = generator.forward(inputs)
+        assert outputs.shape == (1, self.out_features, self.in_features)
+
+
+class TestFacialGraphGenerator:
+    in_features = 5
+    n_main_nodes = 27
+    n_sub_nodes = 14
+    n_neighbors = 1
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, self.n_main_nodes, self.in_features))
+    
+    @pytest.fixture
+    def generator(self):
+        return FacialGraphGenerator(self.in_features, self.n_main_nodes, self.n_sub_nodes, self.n_neighbors)
+    
+
+    def test_forward(self, generator, inputs):
+        with torch.no_grad():
+            outputs = generator.forward(inputs)
+        assert outputs.shape == (1, self.n_main_nodes + self.n_sub_nodes)
+
+
+class TestANFL:
+    in_features = 5
+    n_main_nodes = 27
+    n_sub_nodes = 14
+    n_neighbors = 1
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, 2, self.in_features))
+    
+    @pytest.fixture
+    def anfl(self):
+        return ANFL(self.in_features, self.n_main_nodes, self.n_sub_nodes, self.n_neighbors)
+    
+
+    def test_forward(self, anfl, inputs):
+        with torch.no_grad():
+            outputs = anfl.forward(inputs)
+        assert outputs.shape == (1, self.n_main_nodes + self.n_sub_nodes)
+
+
+class TestMEFARG:
+    in_features = 256
+    n_main_nodes = 27
+    n_sub_nodes = 14
+    n_neighbors = 1
+
+    @pytest.fixture
+    def inputs(self):
+        return torch.rand((1, 3, self.in_features, self.in_features))
+    
+    @pytest.fixture
+    def mefarg(self):
+        return MEFARG(self.n_main_nodes, self.n_sub_nodes, self.n_neighbors)
+
+
+    def test_forward(self, mefarg, inputs):
+        with torch.no_grad():
+            outputs = mefarg.forward(inputs)
+        assert outputs.shape == (1, self.n_main_nodes + self.n_sub_nodes)
+        
