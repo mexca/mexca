b77e1926d4e2fb651255b8f01b8651addfa33b7d
maltelueken <malte_lueken@arcor.de>
2024-05-01 13:38:48 +0200

    Bump to version 1.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ author = "Malte Lüken and Eva Viviani"
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.2"
+version = "1.0.3"
 # The full version, including alpha/beta/rc tags.
 release = version
 
